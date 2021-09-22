1949ac90df1e27e63b92387776620606da8ce16f
Hariharan Thiagarajan <hariharansmm@gmail.com>
2021-09-23 08:51:11 +1200

    Eighth Commit
    Debug Route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/admin/dashboard');
+    // return redirect('/admin/dashboard');
+    return view('welcome');
 });
 
 Auth::routes();
